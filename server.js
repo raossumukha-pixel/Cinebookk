aabfddec1cecaa0b00fa57906709ab86c95ab806
Sumukha <raossumukha@gmail.xom>
2026-07-29 16:27:58 +0530

    Connected Railway MYSQL

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ app.get("/", (req,res)=>{
 
 // MySQL Connection
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"kyliejenner1013!",
-    database:"cinebook"
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 db.connect((err)=>{
