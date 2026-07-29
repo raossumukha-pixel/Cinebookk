bb541347ee226c0d86d39722ab7949b4b3d086bd
Sumukha <raossumukha@gmail.xom>
2026-07-29 16:32:15 +0530

    Use railway

--- server.js
+++ server.js
@@ -12,12 +12,23 @@ app.get("/", (req,res)=>{
 });
 
 // MySQL Connection
+const mysql = require("mysql2");
+
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("Database connection failed");
+    console.log(err);
+  } else {
+    console.log("MySQL Connected");
+  }
 });
 
 db.connect((err)=>{
