67d7d8bc78c8f8a690afd7cf3b0471a84495df17
Sumukha <raossumukha@gmail.xom>
2026-07-29 16:37:41 +0530

    changes done

--- server.js
+++ server.js
@@ -15,20 +15,10 @@ app.get("/", (req,res)=>{
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log("Database connection failed");
-    console.log(err);
-  } else {
-    console.log("MySQL Connected");
-  }
+    host:"localhost",
+    user:"root",
+    password:"kyliejenner1013!",
+    database:"cinebook"
 });
 
 db.connect((err)=>{
